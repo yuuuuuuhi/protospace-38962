bf31d1e7133f82445926df536fbd85fbb5086389
yuuuuuuhi <you.hi.h.s.n.28@outlook.jp>
2023-07-30 17:13:13 +0900

    修正2
    ・ログアウト状態のユーザーであっても、トップページ・プロトタイプ詳細ページ・ユーザー詳細ページ・ユーザー新規登録ページ・ログインページには遷移できること

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,7 +29,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    
     unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
@@ -37,7 +36,6 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
